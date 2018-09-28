93d39c22a17c5985ee598aee5211a7f2ef451526
Chase Whitney <ChaseEWhitney@gmail.com>
2018-09-28 00:25:54 -0500

    added aboutPage and simple route handling, not hooked up to anything

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This application uses the following technologies:
+      </p>
+        <ul>
+          <li>React</li>
+          <li>React Router</li>
+          <li>Flux (maybe?)</li>
+          <li>React-Create-App</li>
+          <li>Node</li>
+          <li>Bootstrap</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,28 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import Home from './components/homePage';
+import About from './components/about/aboutPage';
 
+const App = (props) => {
+  var Child;
+  switch(props.route){
+    case 'about' : Child = About; break;
+    default : Child = Home;
+  }
 
+  return (
+    <div>
+      <Child />
+    </div>
+  )
+}
 
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
+const render = () => {
+  let route = window.location.hash.substr(1);
+  console.log("route:", window.location.hash.substr(1));
+  ReactDOM.render(<App route={route}/>, document.getElementById('root'));
+  registerServiceWorker();
+}
+
+window.addEventListener('hashchange', render());
+render();
